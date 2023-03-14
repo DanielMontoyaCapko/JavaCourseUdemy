7f4e051e57415bb512781290a7f31b5a8571b938
DanielMontoyaCapko <105721571+DanielMontoyaCapko@users.noreply.github.com>
2023-03-14 16:12:55 +0100

    Create PrimeNumberChallenge.java

---
+++ Code&Exercises/6. Control Flow/Loops/For Loop/PrimeNumberChallenge.java
@@ -0,0 +1,26 @@
+public class PrimeNumberChallenge {
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int numList = 1; numList <= 1000; numList++){
+            if (isPrime(numList)){
+                System.out.println(numList + " is a prime number");
+                counter++;
+                if (counter == 3){
+                    break;
+                }
+            }
+        }
+    }
+    public static boolean isPrime(int number){
+        if(number <= 2){
+            return (number == 2);
+        }
+
+        for (int divisor = 2; divisor < number; divisor++){
+            if (number % divisor == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
