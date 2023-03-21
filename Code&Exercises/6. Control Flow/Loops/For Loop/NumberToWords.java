0f9e0c78a00f96651b1a82c2c0585375069e4d58
DanielMontoyaCapko <105721571+DanielMontoyaCapko@users.noreply.github.com>
2023-03-21 16:26:57 +0100

    Create NumberToWords.java

---
+++ Code&Exercises/6. Control Flow/Loops/For Loop/NumberToWords.java
@@ -0,0 +1,81 @@
+public class NumberToWords {
+    public static void main(String[] args) {
+        System.out.println(getDigitCount(0));
+        System.out.println(getDigitCount(123));
+        System.out.println(getDigitCount(-12));
+        System.out.println(getDigitCount(5200));
+
+        System.out.println(reverse(-121));
+        System.out.println(reverse(1212));
+        System.out.println(reverse(1234));
+        System.out.println(reverse(100));
+    }
+
+    public static void numberToWords(int number) {
+        if (number < 0){
+            System.out.println("Invalid Value");
+        }
+
+        int reverseNumber = reverse(number);
+        for (int i = 0; i < getDigitCount(number); i++) {
+
+            switch (reverseNumber % 10) {
+                case 0:
+                    System.out.print("Zero ");
+                    break;
+                case 1:
+                    System.out.print("One ");
+                    break;
+                case 2:
+                    System.out.print("Two ");
+                    break;
+                case 3:
+                    System.out.print("Three ");
+                    break;
+                case 4:
+                    System.out.print("Four ");
+                    break;
+                case 5:
+                    System.out.print("Five ");
+                    break;
+                case 6:
+                    System.out.print("Six ");
+                    break;
+                case 7:
+                    System.out.print("Seven ");
+                    break;
+                case 8:
+                    System.out.print("Eight ");
+                    break;
+                case 9:
+                    System.out.print("Nine ");
+                    break;
+                default:
+                    break;
+            }
+            reverseNumber /= 10;
+        }
+        System.out.println();
+    }
+
+    public static int reverse(int number) {
+        int reverse = 0;
+        while (number != 0){
+            int lastDigit = number % 10;
+            reverse = reverse * 10 + lastDigit;
+            number /= 10;
+        }
+        return reverse;
+    }
+
+    public static int getDigitCount(int number) {
+        if (number < 0) return -1;
+
+        int counter = 1;
+        while (number > 9) {
+            number /= 10;
+            counter++;
+        }
+        return counter;
+    }
+}
