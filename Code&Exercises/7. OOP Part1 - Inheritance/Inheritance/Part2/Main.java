117a6d4f81cf92c47ed519ff8046b19e82363d5e
DanielMontoyaCapko <105721571+DanielMontoyaCapko@users.noreply.github.com>
2023-04-07 12:43:05 +0200

    Create Main.java

---
+++ Code&Exercises/7. OOP Part1 - Inheritance/Inheritance/Part2/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Generic Animal", "Big", 400);
+        doAnimalStuff(animal, "fast");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog,"Fast");
+
+        Dog yorkie = new Dog("Yorkie", 30);
+        doAnimalStuff(yorkie,"Fast");
+        Dog retriever = new Dog( "Retriever", 65,"Floppy","Swimmer");
+        doAnimalStuff(retriever,"Slow");
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        animal.makesNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+    }
+}
